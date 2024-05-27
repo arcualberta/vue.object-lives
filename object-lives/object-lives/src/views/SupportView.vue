9cda4649faf7932c8078705cbdab1ac148914733
ahalyajupalli <ahalya.jupalli@gmail.com>
2024-05-27 11:07:23 -0600

    Added Support Page

--- object-lives/object-lives/src/views/SupportView.vue
+++ object-lives/object-lives/src/views/SupportView.vue
@@ -1,6 +1,31 @@
 <template>
-    <h1>Support Page</h1>
+  <div class="support-page">
+    <div class="image-container">
+      <img src="../assets/thank-you.jpg" alt="Image 1" class="image" />
+      <img src="../assets/thank-image.gif" alt="Image 2" class="image" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
+<style scoped>
+.support-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #1a1a1a;
+}
+
+.image-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-top: 20px;
+}
+
+.image {
+  max-width: 100%;
+  height: auto;
+}
+</style>
