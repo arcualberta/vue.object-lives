bf48978fa33c10fb4640d3b79dded14c310e15ae
ahalyajupalli <ahalya.jupalli@gmail.com>
2024-05-27 11:42:58 -0600

    Added Project Page

--- object-lives/object-lives/src/views/ProjectView.vue
+++ object-lives/object-lives/src/views/ProjectView.vue
@@ -1,6 +1,122 @@
 <template>
-    <h1>Project Page</h1>
+  <div class="page-container">
+    <div class="image-container">
+      <img
+        src="../assets/Image-asset.gif"
+        alt="Project Image"
+        class="project-image"
+      />
+    </div>
+    <div class="project-block">
+      <div class="project-block-content">
+        <div class="project-html-content">
+          <p>&nbsp;</p>
+          <p>
+            <strong
+              >Object Lives and Global Histories in Northern North
+              America:</strong
+            ><br />
+
+            <em>
+              <span
+                >Networks, Localities, and Material Culture c. 1700s-2000s</span
+              >
+            </em>
+          </p>
+          <p><span>&nbsp;</span></p>
+          <p>
+            <em>
+              <span
+                >Object Lives and Global Histories in Northern North
+                America</span
+              >
+            </em>
+            <span>
+              " is a collaborative research project with Edmonton and Montreal
+              as the geographic hubs for its enquiry. This project will explore
+              the histories of material culture within these regions, linked to
+              wider global flows of influence that arise from trade, colonialism
+              and migration."
+            </span>
+            <span></span>
+            <span>
+              The object histories will be developed through our project’s
+            </span>
+            <span>
+              " multi-disciplinary partnership, generating essential new
+              knowledge about people and object interactions. The stories of the
+              selected objects will be presented on the ‘Object Lives’ website,
+              detailing the biographies of goods that acted as surrogates of
+              early globalization and cross-cultural exchange. &nbsp;"
+            </span>
+          </p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
+
+<style scoped>
+.page-container {
+  text-align: left;
+  background-color: #1a1a1a;
+  color: #fcf9f7;
+}
+
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50vh;
+}
+
+.project-image {
+  display: block;
+  max-width: 80%;
+  max-height: 80%;
+  width: auto;
+  height: auto;
+  background-color: #1a1a1a;
+}
+
+.project-block {
+  padding: 20px;
+  background-color: #1a1a1a;
+  border-radius: 5px;
+  font-family: "Proxima Nova", sans-serif;
+  line-height: 1.6em;
+  letter-spacing: 1px;
+  font-style: normal;
+  font-weight: 100;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-family: "proxima-nova";
+}
+
+.project-block-content {
+  padding: 15px;
+}
+
+.project-html-content p {
+  max-width: 950px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin: 0 auto;
+  font-family: "proxima-nova", sans-serif;
+  line-height: 1.6em;
+  letter-spacing: 1px;
+  font-weight: 100;
+  font-style: normal;
+  font-size: 14px;
+  color: #fcf9f7;
+}
+
+.project-block {
+  margin-top: 20px;
+}
+</style>
