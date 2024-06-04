08ec37bfee6dde37b307df33d678589930d174a3
Sanjana Sanjana <sanjanamarwaha.sm@gmail.com>
2024-06-03 21:07:56 -0600

    Updated the login component

--- object-lives/object-lives/src/main.ts
+++ object-lives/object-lives/src/main.ts
@@ -1,15 +1,28 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
-
-// Import Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const app = createApp(App)
-library.add(fab)
+const pinia = createPinia()
+
+// Add the brand icons and the right-to-bracket icon to the Font Awesome library
+library.add(fab, faRightToBracket)
+
+// Register the FontAwesomeIcon component globally
 app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(router).mount('#app')
+
+// Use the Pinia and Vue Router plugins
+app.use(pinia)
+app.use(router)
+
+// Mount the Vue app
+app.mount('#app')
+
+// app.use(pinia).use(router).mount('#app')
