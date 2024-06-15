8274066ba91eef86d3290d9cc9c32d1300ae72d3
ahalyajupalli <35890348+ahalyajupalli@users.noreply.github.com>
2024-06-15 02:48:03 -0600

    Commit message describing the changes

--- object-lives/object-lives/src/main.ts
+++ object-lives/object-lives/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
+import '@/assets/variables.scss';
 
 // Import Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
